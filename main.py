e9444b0ea31db7586d3a221f0c2a33d2a5da6430
Anamistik <Maltsseva1@yandex.ru>
2024-04-18 17:21:01 +0300

    add one more function

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import random
 import time
 
+LIMON = True
+
 def hello():
     random_num = random.randint(1, 100)
     timestamp = time.time()
     print('Hello world:', random_num, timestamp)
 
+def random_num_print():
+    print('Random num: ', random.randint(20, 200))
+
 if __name__ == "__main__":
-    hello()
+    if LIMON:
+        hello()
+    else:
+        random_num_print()
